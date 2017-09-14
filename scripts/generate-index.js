5b71a714311c937be5aee0d1d0a1a13e6563a11e
Markus Tacker <Markus.Tacker@NordicSemi.no>
2017-09-14 17:21:23 +0200

    chore(codestyle): use standard

--- scripts/generate-index.js
+++ scripts/generate-index.js
@@ -1,30 +1,36 @@
-const models = require('@nrfcloud/models');
+const models = require('@nrfcloud/models')
 
-const {ApiIndex, Link, URLValue} = models;
+const {ApiIndex, Link, URLValue} = models
 
 const index = new ApiIndex([
+    // self
+  new Link(
+        new URLValue('https://meta.mrfcloud.com/'),
+        new URLValue('https://github.com/nRFCloud/meta'),
+        'self'
+    ),
     // AWS IoT endpoints
-    new Link(
+  new Link(
         new URLValue('https://a2n7tk1kp18wix.iot.us-east-1.amazonaws.com'),
         new URLValue('https://aws.amazon.com/iot-platform/'),
         'stage@dev,stage@beta,stage@prod'
     ),
     // REST API
-    new Link(
+  new Link(
         new URLValue('https://hnmr2uba55.execute-api.us-east-1.amazonaws.com/prod'),
         new URLValue('https://nrfcloud.com/#api'),
         'stage@prod'
     ),
-    new Link(
+  new Link(
         new URLValue('https://hnmr2uba55.execute-api.us-east-1.amazonaws.com/beta'),
         new URLValue('https://nrfcloud.com/#api'),
         'stage@beta'
     ),
-    new Link(
+  new Link(
         new URLValue('https://hnmr2uba55.execute-api.us-east-1.amazonaws.com/dev'),
         new URLValue('https://nrfcloud.com/#api'),
         'stage@dev'
-    ),
-]);
+    )
+])
 
-console.log(JSON.stringify(index, '', 2));
+console.log(JSON.stringify(index, '', 2))
